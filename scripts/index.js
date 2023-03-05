b2b841745ec882a1aefc8910e3c66ebd1bbb0904
Sergey Ganus <sergey.ganus@gmail.com>
2023-03-05 18:25:59 +0300

    fix: исправить критические замечания

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 // Получение информации о профиле
 const profile = document.querySelector('.profile');
-let profileName = profile.querySelector('.profile__name');
-let profileDescription = profile.querySelector('.profile__description');
+const profileName = profile.querySelector('.profile__name');
+const profileDescription = profile.querySelector('.profile__description');
 const editProfileButton = profile.querySelector('.profile__edit-button');
 const addPlaceButton = profile.querySelector('.profile__add-button');
 
@@ -11,8 +11,8 @@ const editProfilePopupCloseButton = editProfilePopup.querySelector('.popup__clos
 
 // Получение формы редактирования профиля и её элементов
 const editProfileForm = editProfilePopup.querySelector('.form');
-let formProfileName = editProfileForm.querySelector('.form__input_type_profile-name');
-let formProfileDescription = editProfileForm.querySelector('.form__input_type_profile-description');
+const formProfileName = editProfileForm.querySelector('.form__input_type_profile-name');
+const formProfileDescription = editProfileForm.querySelector('.form__input_type_profile-description');
 
 // Получение модального окна добавления нового места и его элементов
 const addPlacePopup = document.querySelector('.popup_type_add-place');
@@ -20,61 +20,44 @@ const addPlacePopupCloseButton = addPlacePopup.querySelector('.popup__close-butt
 
 // Получение формы добавления нового места и её элементов
 const addPlaceForm = addPlacePopup.querySelector('.form');
-let formPlaceName = addPlaceForm.querySelector('.form__input_type_place-name');
-let formPlaceLink = addPlaceForm.querySelector('.form__input_type_place-link');
+const formPlaceName = addPlaceForm.querySelector('.form__input_type_place-name');
+const formPlaceLink = addPlaceForm.querySelector('.form__input_type_place-link');
 
 // Первоначальный массив мест
 const initialPlaces = [
   {
-    id: 1,
     name: 'Архыз',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
   },
   {
-    id: 2,
     name: 'Челябинская область',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
   },
   {
-    id: 3,
     name: 'Иваново',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
   },
   {
-    id: 4,
     name: 'Камчатка',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
   },
   {
-    id: 5,
     name: 'Холмогорский район',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
   },
   {
-    id: 6,
     name: 'Байкал',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
 
-// Актуальный массив мест
-const actualPlaces = initialPlaces;
-
+// Получение элемента списка мест и шаблона карточки места
 const placesListElem = document.querySelector('.places__list');
-const placeTemplate = placesListElem.querySelector('#place').content;
+const placeTemplate = document.querySelector('#place').content;
 
-// Функция рендеринга мест
-function renderPlaces(places) {
-  places.forEach((place) => {
-    renderPlace(place, true);
-  });
-}
-
-// Функция рендеринга одного места
-function renderPlace(place, isAppend = false) {
+// Функция создания места
+function createPlace(place) {
   const placeElem = placeTemplate.querySelector('.place').cloneNode(true);
-
-  placeElem.querySelector('.place__id').value = place.id;
 
   const placeImageElem = placeElem.querySelector('.place__image');
   placeImageElem.src = place.link;
@@ -83,75 +66,102 @@ function renderPlace(place, isAppend = false) {
 
   placeElem.querySelector('.place__title').textContent = place.name;
 
+  placeElem.querySelector('.place__favorite-button').addEventListener('click', togglePlaceLike);
+  placeElem.querySelector('.place__delete-button').addEventListener('click', deletePlace);
+  placeImageElem.addEventListener('click', showGallery);
+
+  return placeElem;
+}
+
+// Функция добавления места
+function addPlace(place, isAppend = false) {
+  const placeElem = createPlace(place);
+
+  // Добавляем элемент в конец или начало списка мест
   if (isAppend) {
     placesListElem.append(placeElem);
   } else {
     placesListElem.prepend(placeElem);
   }
-
-  return placeElem;
 }
 
-// Обработчик открытия и закрытия модального окна
-function togglePopup(popup) {
-  const popupType = popup.classList[1];
+// Функция добавления мест
+function addPlaces(places) {
+  places.forEach((place) => {
+    addPlace(place, true);
+  });
+}
 
-  switch (popupType) {
-    case 'popup_type_edit-profile':
-      if (popup.classList.contains('popup_opened')) {
-        formProfileName.value = '';
-        formProfileDescription.value = '';
-      } else {
-        formProfileName.value = profileName.textContent;
-        formProfileDescription.value = profileDescription.textContent;
-      }
-      popup.classList.toggle('popup_opened');
-      break;
-    case 'popup_type_add-place':
-      if (popup.classList.contains('popup_opened')) {
-        formPlaceName.value = '';
-        formPlaceLink.value = '';
-      }
-      popup.classList.toggle('popup_opened');
-      break;
-    default:
-      console.log('Ошибка: Тип модального окна не определён!');
-      break;
+// Функция заполнения полей формы из профиля
+function setFormProfileData() {
+  formProfileName.value = profileName.textContent;
+  formProfileDescription.value = profileDescription.textContent;
+}
+
+// Функция заполнения профиля из актуальных данных формы
+function setProfileData() {
+  profileName.textContent = formProfileName.value;
+  profileDescription.textContent = formProfileDescription.value;
+}
+
+// Функция заполнения данных галереи
+function setGalleryPopupData(placeImageElem) {
+  const placeElem = placeImageElem.closest('.place');
+  const placeTitleElem = placeElem.querySelector('.place__title');
+  galleryPopupImage.src = placeImageElem.src;
+  galleryPopupImage.alt = placeImageElem.alt;
+  galleryPopupImage.title = placeImageElem.title;
+  galleryPopupTitle.textContent = placeTitleElem.textContent;
+}
+
+// Функция очистки полей формы изменения профиля
+function clearFormProfileData() {
+  formProfileName.value = '';
+  formProfileDescription.value = '';
+}
+
+// Функция очистки полей формы добавления места
+function clearFormPlaceData() {
+  formPlaceName.value = '';
+  formPlaceLink.value = '';
+}
+
+
+// Обработчик открытия модального окна
+function openPopup(popup) {
+  if (popup.classList.contains('popup_type_edit-profile')) {
+    setFormProfileData();
   }
+
+  popup.classList.add('popup_opened');
+}
+
+// Обработчик закрытия модального окна
+function closePopup(popup) {
+  if (popup.classList.contains('popup_type_add-place')) {
+    clearFormPlaceData();
+  }
+
+  popup.classList.remove('popup_opened');
 }
 
 // Получение окна галереи
-const gallery = document.querySelector('.gallery');
-const galleryCloseButton = gallery.querySelector('.gallery__close-button');
-const galleryImage = gallery.querySelector('.gallery__image');
-const galleryTitle = gallery.querySelector('.gallery__title');
+const galleryPopup = document.querySelector('.popup_type_gallery');
+const galleryPopupCloseButton = galleryPopup.querySelector('.popup__close-button');
+const galleryPopupImage = galleryPopup.querySelector('.popup__image');
+const galleryPopupTitle = galleryPopup.querySelector('.popup__title');
 
-// Обработчик открытия и закрытия галереи
-function toggleGallery(evt) {
+// Обработчик открытия модального окна галереи
+function showGallery(evt) {
   const placeImageElem = evt.target;
 
-  if (gallery.classList.contains('gallery_opened')) {
-    galleryImage.alt = 'Фото';
-    galleryImage.title = 'Фото';
-    galleryTitle.textContent = '';
-    gallery.classList.toggle('gallery_opened');
-  } else {
-    const placeElem = placeImageElem.closest('.place');
-    const placeTitleElem = placeElem.querySelector('.place__title');
-    galleryImage.src = placeImageElem.src;
-    galleryImage.alt = placeImageElem.alt;
-    galleryImage.title = placeImageElem.title;
-    galleryTitle.textContent = placeTitleElem.textContent;
-    gallery.classList.toggle('gallery_opened');
-  }
-
+  setGalleryPopupData(placeImageElem);
+  openPopup(galleryPopup);
 }
 
 // Обработчик лайка и отмены лайка месту
 function togglePlaceLike(evt) {
-  if (evt.target.classList.contains('place__favorite-button')) {
-    evt.target.classList.toggle('place__favorite-button_active');
-  }
+  evt.target.classList.toggle('place__favorite-button_active');
 }
 
 // Обработчик сохранения информации о профиле
@@ -160,36 +170,34 @@ function saveProfile(evt) {
   evt.preventDefault();
 
   // Запись актуальных значений в профиль
-  profileName.textContent = formProfileName.value;
-  profileDescription.textContent = formProfileDescription.value;
+  setProfileData();
+
+  // Очистка полей формы
+  clearFormProfileData();
 
   // Закрытие модального окна
-  togglePopup(editProfilePopup);
+  closePopup(editProfilePopup);
 }
 
 // Функция добавления нового места
-function addPlace(evt) {
+function addPlaceHandler(evt) {
   // Отмена стандартной отправки формы
   evt.preventDefault();
 
-  const placesIds = actualPlaces.map(place => place.id);
-  const newId = Math.max(...placesIds) + 1;
-
+  // Создание нового места
   const newPlace = {
-    id: newId,
     name: formPlaceName.value,
     link: formPlaceLink.value
   };
 
-  // Добавление нового элемента, рендер и добавление обработчиков
-  actualPlaces.unshift(newPlace);
-  const newPlaceElem = renderPlace(newPlace);
-  newPlaceElem.querySelector('.place__favorite-button').addEventListener('click', togglePlaceLike);
-  newPlaceElem.querySelector('.place__delete-button').addEventListener('click', deletePlace);
-  newPlaceElem.querySelector('.place__image').addEventListener('click', toggleGallery);
+  // Добавление нового места в DOM
+  addPlace(newPlace);
+
+  // Очистка полей формы
+  clearFormPlaceData();
 
   // Закрытие модального окна
-  togglePopup(addPlacePopup);
+  closePopup(addPlacePopup);
 }
 
 // Функция удаления места
@@ -197,40 +205,22 @@ function deletePlace(evt) {
   const placeDeleteButton = evt.target;
 
   const deletedPlaceElem = placeDeleteButton.closest('.place');
-  const deletedId = deletedPlaceElem.querySelector('.place__id').value;
-
-  for (let i = 0; i < actualPlaces.length; i++) {
-    if (actualPlaces[i].id === Number(deletedId)) {
-      actualPlaces.splice(i, 1);
-    }
-  }
-
   deletedPlaceElem.remove();
 }
 
 // Обработчики событий для редактирования профиля
-editProfileButton.addEventListener('click', () => togglePopup(editProfilePopup));
-editProfilePopupCloseButton.addEventListener('click', () => togglePopup(editProfilePopup));
+editProfileButton.addEventListener('click', () => openPopup(editProfilePopup));
+editProfilePopupCloseButton.addEventListener('click', () => closePopup(editProfilePopup));
 editProfileForm.addEventListener('submit', saveProfile);
 
 // Обработчики событий для добавления нового места
-addPlaceButton.addEventListener('click', () => togglePopup(addPlacePopup));
-addPlacePopupCloseButton.addEventListener('click', () => togglePopup(addPlacePopup));
-addPlaceForm.addEventListener('submit', addPlace);
+addPlaceButton.addEventListener('click', () => openPopup(addPlacePopup));
+addPlacePopupCloseButton.addEventListener('click', () => closePopup(addPlacePopup));
+addPlaceForm.addEventListener('submit', addPlaceHandler);
 
-// Запуск рендера мест
-renderPlaces(initialPlaces);
+// Обработчик события для закрытия галереи
+galleryPopupCloseButton.addEventListener('click', () => closePopup(galleryPopup));
 
-// Получаем все кнопки с лайков и вешаем обработчики
-const placeFavoriteButtons = document.querySelectorAll('.place__favorite-button');
-placeFavoriteButtons.forEach(currFavoriteButton => currFavoriteButton.addEventListener('click', togglePlaceLike));
-
-// Получаем все кнопки удаления мест и вешаем обработчики
-const placeDeleteButtons = document.querySelectorAll('.place__delete-button');
-placeDeleteButtons.forEach(currDeleteButton => currDeleteButton.addEventListener('click', deletePlace));
-
-// Получаем все картинки мест и вешаем обработчики
-const placeImages = document.querySelectorAll('.place__image');
-placeImages.forEach(currPlaceImage => currPlaceImage.addEventListener('click', toggleGallery));
-galleryCloseButton.addEventListener('click', toggleGallery);
+// Запуск функции добавления мест
+addPlaces(initialPlaces);
 
